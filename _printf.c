0b1cadf15cdf41066cdf8a299280c0731f22930f
gnnrbachelor <gnnrbachelor@gmail.com>
2021-03-17 16:17:37 +0000

    Fix count.

--- _printf.c
+++ _printf.c
@@ -32,7 +32,10 @@ int _printf(const char *format, ...)
 		if (format[i + 1])
 			specifier = match_spec(&format[i + 1]);
 		if (!specifier)
+		{
 			_putchar(format[i]);
+			j++;
+		}
 
 		if (specifier)
 		{

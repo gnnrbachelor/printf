e1602d50b67a74043bf0edfec1a2367f6a2a572d
gnnrbachelor <gnnrbachelor@gmail.com>
2021-03-17 13:28:58 +0000

    Separate functions into separate files.

---
+++ char_func.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * print_c - Prints character
+ *
+ * @c: Pointer to arg
+ *
+ * Return: 1
+ */
+
+int print_c(va_list c)
+{
+	_putchar((char)va_arg(c, int));
+	return (1);
+}
+
+/**
+ * print_s - Prints string
+ *
+ * @s: Pointer to arg
+ *
+ * Return: # of characters in string
+ */
+
+int print_s(va_list s)
+{
+	char *str = va_arg(s, char *);
+	int i = 0;
+
+	if (str == NULL)
+		str = "(null)";
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	return (i);
+}
--- num_func.c
+++ num_func.c
@@ -2,44 +2,6 @@
 #include <stdlib.h>
 
 /**
- * print_c - Prints character
- *
- * @c: Pointer to arg
- *
- * Return: 1
- */
-
-int print_c(va_list c)
-{
-	_putchar((char)va_arg(c, int));
-	return (1);
-}
-
-/**
- * print_s - Prints string
- *
- * @s: Pointer to arg
- *
- * Return: # of characters in string
- */
-
-int print_s(va_list s)
-{
-	char *str = va_arg(s, char *);
-	int i = 0;
-
-	if (str == NULL)
-		str = "(null)";
-
-	while (str[i])
-	{
-		_putchar(str[i]);
-		i++;
-	}
-	return (i);
-}
-
-/**
  * print_percent - prints % sign
  *
  * @p: Pointer to arg
